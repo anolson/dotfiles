6c49dba1440d2328508f6d21bf11572f94daae17
Andrew Olson <anolson@gmail.com>
2015-01-24 23:47:32 -0500

    Uno mas.

--- sublime/install.sh
+++ sublime/install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+symlink() {
+  source_file="$1"
+  target_file="$2"
+  echo " - Linking $target_file"
+  ln -sf "$source_file" "$target_file"
+}
+
 FILES="$HOME/.dotfiles/sublime/Packages/User/*"
 SUBLIME_PREFERENCES_DIR="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
 
@@ -8,8 +15,5 @@ echo "Setting up Sublime Text preferences."
 for file in $FILES
 do
   basename=$(basename "$file")
-  source_file="$file"
-  target_file="$SUBLIME_PREFERENCES_DIR/$basename"
-  echo " - Linking $target_file"
-  ln -sf "$source_file" "$target_file"
+  symlink "$file" "$SUBLIME_PREFERENCES_DIR/$basename"
 done
