859b518a08b0a516468c4afc7c68ca0f21c24325
Andrew Olson <anolson@gmail.com>
2017-10-30 12:04:59 -0400

    Add nvm.

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -5,6 +5,9 @@ if which rbenv &>/dev/null ; then
   eval "$(rbenv init -)"
 fi
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
 
