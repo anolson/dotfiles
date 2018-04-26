3e70372d8ade641b77b68d34d006201d82b81857
Andrew Olson <anolson@gmail.com>
2018-04-26 10:32:47 -0700

    Update cdpath.

--- zsh/configs/options.zsh
+++ zsh/configs/options.zsh
@@ -1,3 +1,9 @@
 # autocomplete cd
 setopt auto_cd
-cdpath=($HOME/Code $HOME/Code/go $HOME/Code/javascript $HOME/Code/rails $HOME/Code/ruby)
+cdpath=(
+  $HOME/Code
+  $HOME/Code/go/src/github.com/goabstract
+  $HOME/Code/javascript
+  $HOME/Code/rails
+  $HOME/Code/ruby
+)
