70456fb63f1790f76d9c0a2b1494c9f02d41b326
Andrew Olson <anolson@gmail.com>
2019-10-10 10:00:39 -0400

    Clean-up zsh path config.

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,12 +1,19 @@
+DOTBIN="$HOME/.bin"
+GOBIN="$GOPATH/bin"
+PYBIN="$HOME/Library/Python/2.7/bin"
+LOCALSBIN="/usr/local/sbin"
+
 # load rbenv if available
 if which rbenv &>/dev/null ; then
   eval "$(rbenv init -)"
 fi
 
+# load nvm if available
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
-PATH="$HOME/.bin:$GOPATH/bin:/usr/local/sbin:$HOME/Library/Python/2.7/bin:$PATH"
+# add various other bin dirs
+PATH="$DOTBIN:$GOBIN:$PYBIN:$LOCAlSBIN:$PATH"
 
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
