54371734cfbf5b75daba8791bae81ee58eec5b7a
Andrew Olson <anolson@gmail.com>
2019-10-10 09:42:21 -0400

    Add python bin to $PATH

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -6,7 +6,7 @@ fi
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
-PATH="$HOME/.bin:$GOPATH/bin:/usr/local/sbin:$PATH"
+PATH="$HOME/.bin:$GOPATH/bin:/usr/local/sbin:$HOME/Library/Python/2.7/bin:$PATH"
 
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
