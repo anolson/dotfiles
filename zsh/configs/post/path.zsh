0c41f0a8bbb6c70262da0a927e63a6dfeb10bd6b
Andrew Olson <anolson@gmail.com>
2025-07-17 16:58:02 -0400

    Remove Calendly tooling

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,6 +1,5 @@
 DOTBIN="$HOME/.bin"
 GOBIN="$GOPATH/bin"
-CRTHOME="$HOME/.crt"
 
 # load rbenv if available
 if which rbenv &>/dev/null ; then
@@ -11,14 +10,8 @@ fi
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
-# load rust if available
-[ -s "$HOME/.cargo/env" ] && . "$HOME/.cargo/env"
-
-# Setup PATH for the Google Cloud SDK.
-[[ -f "$HOME/google-cloud-sdk/path.zsh.inc" ]] && . "$HOME/google-cloud-sdk/path.zsh.inc"
-
 # add various other bin dirs
-PATH="$DOTBIN:$GOBIN:$CRTHOME:$PATH"
+PATH="$DOTBIN:$GOBIN:$PATH"
 
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
